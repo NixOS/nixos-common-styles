8f630a83e38e26c921f59a4b253210bf469f82b9
Rok Garbas <rok@garbas.si>
2021-03-31 23:01:42 +0200

    Conventions page added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: "^on[A-Z].*"},
+  options: {
+    storySort: {
+      order: [
+        "Introduction",
+        "Conventions",
+      ],
+    },
+  },
 }
