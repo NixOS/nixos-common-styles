e2ef4ff19b51926d3cd6ca12fcb9c732395e7f0a
Rok Garbas <rok@garbas.si>
2021-04-01 00:13:50 +0200

    providing SVG_FILES via webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,28 @@
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
+
+/*! fromentries. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
+function fromEntries (iterable) {
+  return [...iterable].reduce((obj, [key, val]) => {
+    obj[key] = val
+    return obj
+  }, {})
+}
+
+const SVG_FILES = fromEntries(glob
+  .sync(__dirname + "/../src/assets/*.svg")
+  .map(svg_file => {
+    const key = "SVG_" + (path
+      .basename(svg_file)
+      .toUpperCase()
+      .replace(/[^A-Z0-9]/g, "_")
+      .replace(/_SVG$/, "")
+    );
+    const content = "'" + fs.readFileSync(svg_file).toString() + "'";
+    return [key, content];
+  }));
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -16,7 +41,26 @@ module.exports = {
     // LESS
     config.module.rules.push({
       test: /\.less$/,
-      use: ['style-loader', 'css-loader', 'less-loader'],
+      use: [
+        {
+          loader: 'style-loader'
+        }, 
+        {
+          loader: 'css-loader',
+        },
+        {
+          loader: 'less-loader',
+          options: {
+            lessOptions: {
+              math: "always",
+              globalVars: SVG_FILES,
+              modifyVars: {
+                "path-fonts": "./",
+              }
+            }
+          }
+        },
+      ],
     });
     config.resolve.extensions.push(".less");
 
